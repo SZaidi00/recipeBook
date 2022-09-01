95537e118374b37a0f6cbe67b9a3cdcdc9de04af
Shumail Zaidi <43276288+SZaidi00@users.noreply.github.com>
2022-08-31 23:15:09 -0400

    rough working of sign in

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,21 +1,29 @@
 import firebase, { initializeApp } from "firebase/app"
 import {getAuth} from "firebase/auth"
 
-const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
-})
+
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID
+// }
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQfrovuTBiVowkxunqUSmMW3AFpMGf7So",
+  authDomain: "recipebook-cb863.firebaseapp.com",
+  projectId: "recipebook-cb863",
+  storageBucket: "recipebook-cb863.appspot.com",
+  messagingSenderId: "220674524847",
+  appId: "1:220674524847:web:13f845d09dfc97f702365a"
+};
 
 
 
+const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app)
-
-export default app
-
+export const auth = getAuth(app);
 
