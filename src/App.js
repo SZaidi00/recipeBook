630b36855e4a23cc88d0d87dc0157d6eba3f4b70
Shumail Zaidi <43276288+SZaidi00@users.noreply.github.com>
2022-09-04 13:45:41 -0400

    cleaning up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import { useState } from "react";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut,
-} from "firebase/auth";
 import "./App.css";
-import { auth } from "./firebase-config";
+
 
 
 import LoginRegs from "./Login-Register/LoginRegs";
